30b5b3deef5d730bbd3df6a7623e97e4feae0d63
cindylll022 <cindyllllll022@gmail.com>
2024-10-22 22:56:57 -0700

    update code

--- api/simplify.js
+++ api/simplify.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 
 export default async function handler(req, res) {
   // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.washingtonpost.com/privacy-policy/'); 
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
